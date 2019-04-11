b3128a2cf3b88f76c2ea7b052a8cfe1944f38ad0
tianzhefang <ja_net@163.com>
2019-04-11 14:53:28 +0800

    调整规则，使支持多行程打印（最大支持9行） 第5行和第7行在制作模板的时候有点偏差，放大可看到，打印看没问题

--- src/com/zt/didi/GUIPDF.java
+++ src/com/zt/didi/GUIPDF.java
@@ -13,16 +13,38 @@ public class GUIPDF {
 }
 
 class PDFInterface extends JFrame {
-//    JLabel label_remark = new JLabel("（请使用【退出】按钮，不要使用右上角的【X】，否则无法关闭JAVA进程）");
+  static String sepChar = ",";
 
-    JLabel lable_root_path = new JLabel("根目录，不可为C盘下位置 (eg: D:)");
-    JTextField input_root_path = new JTextField(1);
-    JLabel label_apply_date = new JLabel("申请日期 (eg: yyyy-mm-dd)");
-    JTextField input_apply_date = new JTextField(10);
+  Map<String,String> didiInfoMap = new HashMap();
+
+  String travelInfoKey = "";
+
+  String travel_type = "";
+  String travel_date = "";
+  String amount = "";
+  String time = "";
+  String weekday = "";
+  String city = "";
+  String address_from = "";
+  String address_to = "";
+  String miles = "";
+
+  String root_path = "";
+  String apply_date = "";
+  String phone = "";
+
+
+  //    JLabel label_remark = new JLabel("（请使用【退出】按钮，不要使用右上角的【X】，否则无法关闭JAVA进程）");
+    JPanel buttonHeadPanel = new JPanel();
+    JButton btn_Reset = new JButton("重置");
+    JButton btn_SaveAsTemp = new JButton("存为样板");
+    JButton btn_LoadTemp = new JButton("从样板载入");
+
+    JPanel inputTravelInfopanel = new JPanel(new GridLayout(9,2));
+    JLabel label_travel_type = new JLabel("乘车类型(eg: 快车,专车,顺风车)");
+    JTextField input_travel_type = new JTextField(10);
     JLabel label_travel_date = new JLabel("行程时间 (eg: yyyy-mm-dd)");
     JTextField input_travel_date = new JTextField(10);
-    JLabel label_phone = new JLabel("联系电话 (eg: 1xxxxxxxxxx)");
-    JTextField input_phone = new JTextField(10);
     JLabel label_amount = new JLabel("行程金额 (eg: xxx.xx)");
     JTextField input_amount = new JTextField(10);
     JLabel label_time = new JLabel("出行时间 (eg: hh:mm)");
@@ -38,116 +60,98 @@ class PDFInterface extends JFrame {
     JLabel label_miles = new JLabel(" 里  程  (eg: 参照软件xx.x)");
     JTextField input_miles = new JTextField(10);
 
-    JButton button = new JButton("生成PDF");
-    JButton button1 = new JButton("重置");
-    JButton btn_SaveAsTemp = new JButton("存为样板");
-    JButton btn_LoadTemp = new JButton("从样板载入");
+    JPanel buttonAddPanel = new JPanel();
+    JButton btn_Add = new JButton("添加行程");
 
-    JTextArea text_result = new JTextArea("...");
+    JPanel showTravelInfoPanel = new JPanel();
+  JTextArea text_TravelInfo = new JTextArea("");
+//    JTextArea text_TravelInfo = new JTextArea("...1111111111111111111111111111111111111111111111111111");
+
+    JPanel inputCommonInfopanel = new JPanel(new GridLayout(3,2));
+    JLabel lable_root_path = new JLabel("根目录，不可为C盘下位置 (eg: D:)");
+    JTextField input_root_path = new JTextField(1);
+    JLabel label_apply_date = new JLabel("申请日期 (eg: yyyy-mm-dd)");
+    JTextField input_apply_date = new JTextField(10);
+    JLabel label_phone = new JLabel("联系电话 (eg: 1xxxxxxxxxx)");
+    JTextField input_phone = new JTextField(10);
+
+    JPanel buttonGenPanel = new JPanel();
+    JButton btn_Gen = new JButton("生成PDF");
+
+    JPanel showMessagePanel = new JPanel();
+    JTextArea text_result = new JTextArea("");
 
     public PDFInterface(){
 
-//        JPanel resultPanel = new JPanel();
-//        resultPanel.add(text_result);
-//        this.add(resultPanel);
+        buttonHeadPanel.add(btn_Reset);
+        buttonHeadPanel.add(btn_SaveAsTemp);
+        buttonHeadPanel.add(btn_LoadTemp);
+        this.add(buttonHeadPanel);
 
-        JPanel buttonpanel = new JPanel();
-        buttonpanel.add(button);
-        buttonpanel.add(button1);
-        buttonpanel.add(btn_SaveAsTemp);
-        buttonpanel.add(btn_LoadTemp);
-        this.add(buttonpanel);
 
-        JPanel inputpanel = new JPanel(new GridLayout(12,2));
+        input_travel_type.setText("快车");
+        inputTravelInfopanel.add(input_travel_type);
+        inputTravelInfopanel.add(label_travel_type);
+        inputTravelInfopanel.add(input_travel_date);
+        inputTravelInfopanel.add(label_travel_date);
+        inputTravelInfopanel.add(input_time);
+        inputTravelInfopanel.add(label_time);
+        inputTravelInfopanel.add(input_weekday);
+        inputTravelInfopanel.add(label_weekday);
+        inputTravelInfopanel.add(input_city);
+        inputTravelInfopanel.add(label_city);
+        inputTravelInfopanel.add(input_address_from);
+        inputTravelInfopanel.add(label_address_from);
+        inputTravelInfopanel.add(input_address_to);
+        inputTravelInfopanel.add(label_address_to);
+        inputTravelInfopanel.add(input_miles);
+        inputTravelInfopanel.add(label_miles);
+        inputTravelInfopanel.add(input_amount);
+        inputTravelInfopanel.add(label_amount);
+        this.add(inputTravelInfopanel);
+
+        buttonAddPanel.add(btn_Add);
+        this.add(buttonAddPanel);
+
+        showTravelInfoPanel.add(text_TravelInfo);
+        this.add(showTravelInfoPanel);
 
         input_root_path.setText("D:");
-        inputpanel.add(input_root_path);
-        inputpanel.add(lable_root_path);
-        inputpanel.add(input_apply_date);
-        inputpanel.add(label_apply_date);
-        inputpanel.add(input_travel_date);
-        inputpanel.add(label_travel_date);
-        inputpanel.add(input_phone);
-        inputpanel.add(label_phone);
-        inputpanel.add(input_amount);
-        inputpanel.add(label_amount);
-        inputpanel.add(input_time);
-        inputpanel.add(label_time);
-        inputpanel.add(input_weekday);
-        inputpanel.add(label_weekday);
-        inputpanel.add(input_city);
-        inputpanel.add(label_city);
-        inputpanel.add(input_address_from);
-        inputpanel.add(label_address_from);
-        inputpanel.add(input_address_to);
-        inputpanel.add(label_address_to);
-        inputpanel.add(input_miles);
-        inputpanel.add(label_miles);
-        this.add(inputpanel);
+        inputCommonInfopanel.add(input_root_path);
+        inputCommonInfopanel.add(lable_root_path);
+        inputCommonInfopanel.add(input_apply_date);
+        inputCommonInfopanel.add(label_apply_date);
+        inputCommonInfopanel.add(input_phone);
+        inputCommonInfopanel.add(label_phone);
+        this.add(inputCommonInfopanel);
 
-        JPanel remarkPanel = new JPanel();
-        remarkPanel.add(text_result);
-        this.add(remarkPanel);
+        buttonGenPanel.add(btn_Gen);
+        this.add(buttonGenPanel);
 
-        button.addActionListener(new ActionListener () {
+        showMessagePanel.add(text_result);
+        this.add(showMessagePanel);
+
+        btn_Reset.addActionListener(new ActionListener () {
             public void actionPerformed(ActionEvent e){
 //                inputField.setText(inputField.getText()+"7");
-                Map<String,String> map = new HashMap();
-                map.put("root_path",input_root_path.getText());
-                map.put("apply_date",input_apply_date.getText());
-                String travel_date = input_travel_date.getText();
-                map.put("date_from",travel_date);
-                map.put("date_to",input_travel_date.getText());
-                map.put("phone",input_phone.getText());
-                map.put("count","1");
-                map.put("amount",input_amount.getText());
-
-                String time = input_time.getText();
-                if(travel_date.length()==10){
-                    time = travel_date.substring(5) + " " + time;
-                }
-                map.put("time",time);
-                map.put("week",input_weekday.getText());
-                map.put("city",input_city.getText());
-                map.put("address_from",input_address_from.getText());
-                map.put("address_to",input_address_to.getText());
-                map.put("miles",input_miles.getText());
-
-                boolean isValid = true;
-                Iterator iter = map.entrySet().iterator();
-                while (iter.hasNext()) {
-                    Map.Entry entry = (Map.Entry) iter.next();
-                    Object val = entry.getValue();
-                    if(val==null || "".equals(String.valueOf(val))){
-                        isValid = false;
-                        continue;
-                    }
-                }
-
-                if(isValid){
-                    Map<String,Object> o=new HashMap();
-                    o.put("datemap",map);
-                    GenPDF.pdfout(o);
-                    text_result.setText("生成成功,产生文件路径在：\r\n"+GenPDF.getOutPutPath());
-                }else{
-                    text_result.setText("生成失败,录入项不可为空");
-                }
-            }
-        });
-
-        button1.addActionListener(new ActionListener () {
-            public void actionPerformed(ActionEvent e){
-                input_root_path.setText("D:");
-                input_apply_date.setText("");
+                input_travel_type.setText("快车");
                 input_travel_date.setText("");
-                input_phone.setText("");
-                input_amount.setText("");
                 input_time.setText("");
                 input_weekday.setText("");
-                input_city.setText("北京市");
-                input_address_from.setText("北京理工大学中关村国防科技园");
+                input_city.setText("");
+                input_address_from.setText("");
                 input_address_to.setText("");
                 input_miles.setText("");
+                input_amount.setText("");
+
+                text_TravelInfo.setText("");
+
+                input_root_path.setText("D:");
+                input_apply_date.setText("");
+                input_phone.setText("");
+
+                text_result.setText("");
+
             }
         });
 
@@ -159,18 +163,19 @@ class PDFInterface extends JFrame {
                         FileWriter fw = new FileWriter(file,false);
                         BufferedWriter bw = new BufferedWriter(fw);
 
+                      if(!"".equals(input_travel_type.getText())){bw.write("input_travel_type::::"+input_travel_type.getText()+"\r\n");}
+                      if(!"".equals(input_travel_date.getText())){bw.write("input_travel_date::::"+input_travel_date.getText()+"\r\n");}
+                      if(!"".equals(input_time.getText())){bw.write("input_time::::"+input_time.getText()+"\r\n");}
+                      if(!"".equals(input_weekday.getText())){bw.write("input_weekday::::"+input_weekday.getText()+"\r\n");}
+                      if(!"".equals(input_city.getText())){bw.write("input_city::::"+input_city.getText()+"\r\n");}
+                      if(!"".equals(input_address_from.getText())){bw.write("input_address_from::::"+input_address_from.getText()+"\r\n");}
+                      if(!"".equals(input_address_to.getText())){bw.write("input_address_to::::"+input_address_to.getText()+"\r\n");}
+                      if(!"".equals(input_miles.getText())){bw.write("input_miles::::"+input_miles.getText()+"\r\n");}
+                      if(!"".equals(input_amount.getText())){bw.write("input_amount::::"+input_amount.getText()+"\r\n");}
+
                         if(!"".equals(input_root_path.getText())){bw.write("input_root_path::::"+input_root_path.getText()+"\r\n");}
                         if(!"".equals(input_apply_date.getText())){bw.write("input_apply_date::::"+input_apply_date.getText()+"\r\n");}
-                        if(!"".equals(input_travel_date.getText())){bw.write("input_travel_date::::"+input_travel_date.getText()+"\r\n");}
                         if(!"".equals(input_phone.getText())){bw.write("input_phone::::"+input_phone.getText()+"\r\n");}
-                        if(!"".equals(input_amount.getText())){bw.write("input_amount::::"+input_amount.getText()+"\r\n");}
-                        if(!"".equals(input_time.getText())){bw.write("input_time::::"+input_time.getText()+"\r\n");}
-                        if(!"".equals(input_weekday.getText())){bw.write("input_weekday::::"+input_weekday.getText()+"\r\n");}
-                        if(!"".equals(input_city.getText())){bw.write("input_city::::"+input_city.getText()+"\r\n");}
-                        if(!"".equals(input_address_from.getText())){bw.write("input_address_from::::"+input_address_from.getText()+"\r\n");}
-                        if(!"".equals(input_address_to.getText())){bw.write("input_address_to::::"+input_address_to.getText()+"\r\n");}
-                        if(!"".equals(input_miles.getText())){bw.write("input_miles::::"+input_miles.getText()+"\r\n");}
-                        if(!"".equals(input_apply_date.getText())){bw.write("input_apply_date::::"+input_apply_date.getText()+"\r\n");}
 
                         bw.close(); fw.close();
                         System.out.println("test1 done!");
@@ -188,7 +193,7 @@ class PDFInterface extends JFrame {
                 if(personalTemp.isFile() && personalTemp.exists()){
                     try {
                         FileInputStream fileInputStream = new FileInputStream(personalTemp);
-                        InputStreamReader inputStreamReader = new InputStreamReader(fileInputStream, "GBK");
+                        InputStreamReader inputStreamReader = new InputStreamReader(fileInputStream, "utf-8");
                         BufferedReader bufferedReader = new BufferedReader(inputStreamReader);
 
                         StringBuffer sb = new StringBuffer();
@@ -199,17 +204,19 @@ class PDFInterface extends JFrame {
                             System.out.println(text);
                             if(text!=null && text.indexOf("::::")>=0){
                                 rowInfo = text.split("::::");
-                                if("input_root_path".equals(rowInfo[0])){input_root_path.setText(rowInfo[1]);}
+
+                              if("input_travel_type".equals(rowInfo[0])){input_travel_type.setText(rowInfo[1]);}
+                              else if("input_travel_date".equals(rowInfo[0])){input_travel_date.setText(rowInfo[1]);}
+                              else if("input_time".equals(rowInfo[0])){input_time.setText(rowInfo[1]);}
+                              else if("input_weekday".equals(rowInfo[0])){input_weekday.setText(rowInfo[1]);}
+                              else if("input_city".equals(rowInfo[0])){input_city.setText(rowInfo[1]);}
+                              else if("input_address_from".equals(rowInfo[0])){input_address_from.setText(rowInfo[1]);}
+                              else if("input_address_to".equals(rowInfo[0])){input_address_to.setText(rowInfo[1]);}
+                              else if("input_miles".equals(rowInfo[0])){input_miles.setText(rowInfo[1]);}
+                              else if("input_amount".equals(rowInfo[0])){input_amount.setText(rowInfo[1]);}
+                                else if("input_root_path".equals(rowInfo[0])){input_root_path.setText(rowInfo[1]);}
                                 else if("input_apply_date".equals(rowInfo[0])){input_apply_date.setText(rowInfo[1]);}
-                                else if("input_travel_date".equals(rowInfo[0])){input_travel_date.setText(rowInfo[1]);}
                                 else if("input_phone".equals(rowInfo[0])){input_phone.setText(rowInfo[1]);}
-                                else if("input_amount".equals(rowInfo[0])){input_amount.setText(rowInfo[1]);}
-                                else if("input_time".equals(rowInfo[0])){input_time.setText(rowInfo[1]);}
-                                else if("input_weekday".equals(rowInfo[0])){input_weekday.setText(rowInfo[1]);}
-                                else if("input_city".equals(rowInfo[0])){input_city.setText(rowInfo[1]);}
-                                else if("input_address_from".equals(rowInfo[0])){input_address_from.setText(rowInfo[1]);}
-                                else if("input_address_to".equals(rowInfo[0])){input_address_to.setText(rowInfo[1]);}
-                                else if("input_miles".equals(rowInfo[0])){input_miles.setText(rowInfo[1]);}
                             }
                         }
                         if(sb==null || "".equals(sb.toString())){
@@ -223,10 +230,60 @@ class PDFInterface extends JFrame {
             }
         });
 
+        btn_Add.addActionListener(new ActionListener () {
+            public void actionPerformed(ActionEvent e){
+              travel_type = input_travel_type.getText();
+              travel_date = input_travel_date.getText();
+              time = input_time.getText();
+              weekday = input_weekday.getText();
+              city = input_city.getText();
+              address_from = input_address_from.getText();
+              address_to = input_address_to.getText();
+              miles = input_miles.getText();
+              amount = input_amount.getText();
+
+              travelInfoKey = new StringBuffer("")
+                  .append(travel_type).append(sepChar)
+                  .append(travel_date).append(sepChar)
+                  .append(time).append(sepChar)
+                  .append(weekday).append(sepChar)
+                  .append(city).append(sepChar)
+                  .append(address_from).append(sepChar)
+                  .append(address_to).append(sepChar)
+                  .append(miles).append(sepChar)
+                  .append(amount).append(sepChar)
+                  .append("\r\n")
+                  .toString();
+
+              text_TravelInfo.append(travelInfoKey);
+            }
+        });
+
+        btn_Gen.addActionListener(new ActionListener () {
+            public void actionPerformed(ActionEvent e){
+              root_path = input_root_path.getText();
+              apply_date = input_apply_date.getText();
+              phone = input_phone.getText();
+
+              travel_date = input_travel_date.getText();
+              amount = input_amount.getText();
+              time = input_time.getText();
+
+              didiInfoMap.put("root_path", root_path);
+              didiInfoMap.put("apply_date", apply_date);
+              didiInfoMap.put("phone", phone);
+              didiInfoMap.put("text_TravelInfo", text_TravelInfo.getText());
+
+              didiInfoMap.put("img_path","template/blank.jpg");
+
+              GenPDF.pdfPrint(didiInfoMap);
+            }
+        });
+
         this.setDefaultCloseOperation(EXIT_ON_CLOSE);
         this.setTitle("滴滴 o(*￣︶￣*)o 生成器");
         this.setResizable(false);
-        this.setSize(490,400);
+        this.setSize(490,600);
         this.setLocationRelativeTo(null);
         this.setLayout(new FlowLayout());
         this.setVisible(true);
--- src/com/zt/didi/GenPDF.java
+++ src/com/zt/didi/GenPDF.java
@@ -3,6 +3,7 @@ package com.zt.didi;
 import java.io.ByteArrayOutputStream;
 import java.io.FileOutputStream;
 import java.io.IOException;
+import java.math.BigDecimal;
 import java.util.HashMap;
 import java.util.Map;
 
@@ -13,14 +14,28 @@ import com.itextpdf.text.pdf.*;
  */
 public class GenPDF {
     static String tOutputPDFPath = "";
+    static String sepChar = ",";
+
     // 利用模板生成pdf
-    public static void pdfout(Map<String,Object> o) {
-        Map<String,String> datemap = (Map<String,String>)o.get("datemap");
-//        datemap.put("empty"," ");
+    public static void pdfPrint(Map<String,String> o) {
+        String root_path = o.get("root_path");
+        String apply_date = o.get("apply_date");
+        String phone = o.get("phone");
+        String text_TravelInfo = o.get("text_TravelInfo");
+        String img_Path = o.get("img_path");
+
+        String[] travelInfos = text_TravelInfo.replace("\r\n","\n").split("\n");
+
+        int rows = travelInfos.length;
+
+        String date_from = "";
+        String date_to = "";
+        BigDecimal total_Amount = new BigDecimal("0");
+
         // 模板路径
-        String templatePath = "didi_template.pdf";
+        String templatePath = "template/didi_template.pdf";
         // 生成的新文件路径
-        tOutputPDFPath = datemap.get("root_path")+"/滴滴出行行程报销单_"+(datemap.get("date_from"))+".pdf";
+        tOutputPDFPath = root_path+"/滴滴出行行程报销单_"+apply_date+".pdf";
         PdfReader reader;
         FileOutputStream out;
         ByteArrayOutputStream bos;
@@ -36,29 +51,62 @@ public class GenPDF {
             AcroFields form = stamper.getAcroFields();
             //文字类的内容处理
             form.addSubstitutionFont(bf);
-            for(String key : datemap.keySet()){
-                String value = datemap.get(key);
-                form.setField(key,value);
+
+            for(int i=1; i<=rows; i++){
+              String curTravelInfo = travelInfos[i-1];
+              String[] curTravelMsg = curTravelInfo.split(sepChar);
+              int t_index=0;
+              form.setField("num"+i, String.valueOf(i));
+              form.setField("type"+i, curTravelMsg[t_index++]);
+              form.setField("time"+i, curTravelMsg[t_index++].substring(5,10) + " " + curTravelMsg[t_index++]);
+              form.setField("week"+i, curTravelMsg[t_index++]);
+              form.setField("city"+i, curTravelMsg[t_index++]);
+              form.setField("address_from"+i, curTravelMsg[t_index++]);
+              form.setField("address_to"+i, curTravelMsg[t_index++]);
+              form.setField("miles"+i, curTravelMsg[t_index++]);
+              form.setField("amount"+i, curTravelMsg[t_index++]);
+              if(i==1){
+                  date_from = curTravelMsg[1];
+                  date_to = curTravelMsg[1];
+              }else{
+                  if(date_from.compareTo(curTravelMsg[1])>0){date_from = curTravelMsg[1];}
+                  if(date_to.compareTo(curTravelMsg[1])<0){date_to = curTravelMsg[1];}
+              }
+              total_Amount = total_Amount.add(BigDecimal.valueOf(Double.valueOf(curTravelMsg[8])));
             }
-//            //图片类的内容处理
-//            Map<String,String> imgmap = (Map<String,String>)o.get("imgmap");
-//            for(String key : imgmap.keySet()) {
-//                String value = imgmap.get(key);
-//                String imgpath = value;
-//                int pageNo = form.getFieldPositions(key).get(0).page;
-//                Rectangle signRect = form.getFieldPositions(key).get(0).position;
-//                float x = signRect.getLeft();
-//                float y = signRect.getBottom();
-//                //根据路径读取图片
-//                Image image = Image.getInstance(imgpath);
-//                //获取图片页面
-//                PdfContentByte under = stamper.getOverContent(pageNo);
-//                //图片大小自适应
-//                image.scaleToFit(signRect.getWidth(), signRect.getHeight());
-//                //添加图片
-//                image.setAbsolutePosition(x, y);
-//                under.addImage(image);
-//            }
+            form.setField("apply_date", apply_date);
+            form.setField("date_from", date_from);
+            form.setField("date_to", date_to);
+            form.setField("phone", phone);
+            form.setField("count", String.valueOf(rows));
+            form.setField("total_amount", String.format("%.2f", total_Amount.doubleValue()));
+
+            //图片类的内容处理
+            int pageNo = form.getFieldPositions("img").get(0).page;
+            Rectangle signRect = form.getFieldPositions("img").get(0).position;
+            float x = signRect.getLeft();
+            float y = signRect.getBottom();
+            float wide = signRect.getWidth();
+            float height = signRect.getHeight();
+//          System.out.println(x);
+//          System.out.println(y);
+//          System.out.println(signRect.getWidth());
+//          System.out.println(signRect.getHeight());
+            //根据路径读取图片
+            Image image = Image.getInstance(img_Path);
+            //获取图片页面
+            PdfContentByte under = stamper.getOverContent(pageNo);
+            //图片大小自适应
+//            image.scaleAbsolute(signRect.getWidth(), signRect.getHeight());
+            image.scaleAbsolute(signRect.getWidth(), -signRect.getHeight()*(10-rows));
+//            image.scaleToFit(signRect.getWidth(), signRect.getHeight());
+            //添加图片
+//            image.setAbsolutePosition(x, y);
+            float buffer_y = 0;
+            if(rows==5 || rows==7){buffer_y = -1;}
+            image.setAbsolutePosition(x, y - (rows - 2) * height + buffer_y);
+            under.addImage(image);
+
             stamper.setFormFlattening(true);// 如果为false，生成的PDF文件可以编辑，如果为true，生成的PDF文件不可以编辑
             stamper.close();
             Document doc = new Document();
@@ -81,30 +129,33 @@ public class GenPDF {
     }
 
     public static void main(String[] args) {
+
         //上面数字  carlibri 29
         //下面数字  carlibri 26
         //下面中文  微软雅黑 22
         Map<String,String> map = new HashMap();
-        map.put("root_path","D");
+        map.put("root_path","D:");
         map.put("apply_date","2019-01-01");
-        map.put("date_from","2019-01-02");
-        map.put("date_to","2019-01-02");
         map.put("phone","18518760178");
-        map.put("count","1");
-        map.put("amount","111.11");
-        map.put("time","01-01 21:11");
-        map.put("week","周四");
-        map.put("city","北京市");
-        map.put("address_from","国防科技园");
-        map.put("address_to","天骄骏园");
-        map.put("miles","37");
+
+        String textInfo = "快车,2019-01-01,20:15,周一,北京市,北京理工大学中关村国防科技园,天骄骏园,28.1,85.12"+"\r\n"
+            +"快车,2019-01-02,20:16,周二,北京市,北京理工大学中关村国防科技园,天骄骏园,28.1,85.12"+"\r\n"
+            +"快车,2019-01-03,20:17,周三,北京市,北京理工大学中关村国防科技园,天骄骏园,28.1,85.12"+"\r\n"
+            +"快车,2019-01-04,20:18,周四,北京市,北京理工大学中关村国防科技园,天骄骏园,28.1,85.12"+"\r\n"
+            +"快车,2019-01-05,20:19,周五,北京市,北京理工大学中关村国防科技园,天骄骏园,28.1,85.12"+"\r\n"
+//            +"快车,2019-01-06,20:20,周六,北京市,北京理工大学中关村国防科技园,天骄骏园,28.1,85.12"+"\r\n"
+//            +"快车,2019-01-07,20:21,周日,北京市,北京理工大学中关村国防科技园,天骄骏园,28.1,85.12"+"\r\n"
+//            +"快车,2019-01-08,20:22,周一,北京市,北京理工大学中关村国防科技园,天骄骏园,28.1,85.12"+"\r\n"
+//            +"快车,2019-01-09,20:23,周二,北京市,北京理工大学中关村国防科技园,天骄骏园,28.1,85.12"+"\r\n"
+            + "";
+        map.put("text_TravelInfo",textInfo);
 
 //        Map<String,String> map2 = new HashMap();
-//        map2.put("img","c:/50336.jpg");
+//        map2.put("img","template/blank.jpg");
 
-        Map<String,Object> o=new HashMap();
-        o.put("datemap",map);
 //        o.put("imgmap",map2);
-        pdfout(o);
+        map.put("img_path","template/blank.jpg");
+
+        pdfPrint(map);
     }
 }
